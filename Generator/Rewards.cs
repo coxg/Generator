3d7374a52307fe98d46fe22c80b0d4009ca00ae8
coxg <gavencox@gmail.com>
2019-11-07 23:14:14 -0500

    Adding rewards

---
+++ Generator/Rewards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    public class Rewards
+    {
+        public int Experience;
+        public int ClassPoints;
+        public int Junk;
+        public List<Item> Items;
+
+        public Rewards(int experience=0, int classPoints=0, int junk=0, List<Item> items=null)
+        {
+            Experience = experience;
+            ClassPoints = classPoints;
+            Junk = junk;
+            Items = items ?? new List<Item>();
+        }
+
+        public void Award()
+        {
+            Globals.Party.AddExperience(Experience);
+            Globals.Party.AddClassPoints(ClassPoints);
+            Globals.Party.AddJunk(Junk);
+            foreach (var item in Items)
+            {
+                Globals.Party.AddItem(item);
+            }
+        }
+    }
+}
